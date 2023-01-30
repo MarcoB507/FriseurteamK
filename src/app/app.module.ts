0ded52d9572ebb48b59673a8459b0a36032e9172
mbart <mbarth509@gmail.com>
2023-01-30 18:28:40 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { PlacementComponent } from './placement/placement.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainpageComponent,
-    PlacementComponent
+    PlacementComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
