9dec7b5d7bb15b1b7d876c88a0254ac8998889c4
mbart <mbarth509@gmail.com>
2023-01-30 18:44:46 +0100

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { PlacementComponent } from './placement/placement.component';
 import { ProductsComponent } from './products/products.component';
+import { PricelistComponent } from './pricelist/pricelist.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainpageComponent,
     PlacementComponent,
-    ProductsComponent
+    ProductsComponent,
+    PricelistComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule
