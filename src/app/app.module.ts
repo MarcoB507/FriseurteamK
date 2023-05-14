bdab39751dcfcabdc8a7048d57efa557dbced54b
mbart <mbarth509@gmail.com>
2023-05-14 12:16:25 +0200

    adapted files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainpageComponent } from './mainpage/mainpage.component';
+import { HeaderComponent } from './header/header.component';
 import { PlacementComponent } from './placement/placement.component';
 import { ProductsComponent } from './products/products.component';
 import { PricelistComponent } from './pricelist/pricelist.component';
@@ -11,7 +11,7 @@ import { ImpressumComponent } from './impressum/impressum.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MainpageComponent,
+    HeaderComponent,
     PlacementComponent,
     ProductsComponent,
     PricelistComponent,
