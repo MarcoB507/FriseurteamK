732713413c53352534f5b5f99077e876708f0556
mbart <mbarth509@gmail.com>
2023-07-23 20:00:38 +0200

    re-work mainpage component

--- src/app/components/mainpage/mainpage.component.ts
+++ src/app/components/mainpage/mainpage.component.ts
@@ -1,29 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
   styleUrls: ['./mainpage.component.css']
 })
-export class MainpageComponent {
-  myIndex = 0;
+export class MainpageComponent implements OnInit{
+  slideIndex: number = 1;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.carousel();
+  ngOnInit() {
+    this.showSlides(this.slideIndex);
   }
 
-  carousel() {
-    const x = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
-    for (let i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    const dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-    this.myIndex++;
-    if (this.myIndex > x.length) {
-      this.myIndex = 1;
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
     }
-    x[this.myIndex - 1].style.display = "block";
-    setTimeout(() => this.carousel(), 5000);
+
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
   }
 }
+
+
+
+
+
+
--- src/app/components/placement/placement.component.ts
+++ src/app/components/placement/placement.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-placement',
   templateUrl: './placement.component.html',
   styleUrls: ['./placement.component.css']
 })
-export class PlacementComponent {
+export class PlacementComponent{
 
 }
