65093dd456aa7bff9eae6826bed9561aa4e47e8e
mbart <mbarth509@gmail.com>
2023-08-13 22:44:36 +0200

    adapted placement component: add Overview-table

--- src/app/components/placement/placement.component.ts
+++ src/app/components/placement/placement.component.ts
@@ -5,28 +5,28 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './placement.component.html',
   styleUrls: ['./placement.component.css']
 })
-export class PlacementComponent implements  OnInit{
+export class PlacementComponent {
 
 
-  ngOnInit() {
-    // Beim Initialisieren der Komponente "London" als aktiven Tab aktivieren
-    this.openCity('Herzzentrum');
-  }
-
-  openCity(cityName: string): void {
-    const tabcontent = document.getElementsByClassName('tabcontent') as HTMLCollectionOf<HTMLElement>;
-    for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = 'none';
-    }
-
-    const tablinks = document.getElementsByClassName('tablinks');
-    for (let i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove('active');
-    }
-
-    const cityElement = document.getElementById(cityName);
-    if (cityElement) {
-      cityElement.style.display = 'block';
-    }
-  }
+  // ngOnInit() {
+  //   // Beim Initialisieren der Komponente "London" als aktiven Tab aktivieren
+  //   this.openCity('Herzzentrum');
+  // }
+  //
+  // openCity(cityName: string): void {
+  //   const tabcontent = document.getElementsByClassName('tabcontent') as HTMLCollectionOf<HTMLElement>;
+  //   for (let i = 0; i < tabcontent.length; i++) {
+  //     tabcontent[i].style.display = 'none';
+  //   }
+  //
+  //   const tablinks = document.getElementsByClassName('tablinks');
+  //   for (let i = 0; i < tablinks.length; i++) {
+  //     tablinks[i].classList.remove('active');
+  //   }
+  //
+  //   const cityElement = document.getElementById(cityName);
+  //   if (cityElement) {
+  //     cityElement.style.display = 'block';
+  //   }
+  // }
 }
